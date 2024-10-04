3aafe1cc27ecff684742bbf0657ec76954e260d4
upawar <uday.pawar@learningmate.com>
2024-10-04 16:21:04 +0530

    Added login and signup pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
-import { Stack } from 'expo-router';
+import { Stack,useRouter } from 'expo-router';
+import { useEffect } from 'react';
+// import { Login } from './Login'
 
 export default function RootLayout() {
+  const router = useRouter();
+  useEffect(() => {
+    // Redirect to the Login page when the app mounts
+    router.replace('./Login'); // Assuming the Login page is at /login
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
